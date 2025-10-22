62156a46c50bb5bb8f354a4e530acf50241ac83d
Muhammed Emin Ceylan <emjey0612@example.com>
2025-10-22 16:12:29 +0300

    Projeye ekleme yapıldı

--- calculator.py
+++ calculator.py
@@ -15,3 +15,22 @@ while True:
         print("Result:",result(first_value,process,second_value))
     except ValueError:
         print("Please enter a valid number!!")
+
+
+    import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    import os 
+        def result(a, b, operator):
+    if operator == "+":
+        return add(a, b)
+    elif operator == "-":
+        return subtract(a, b)
+    elif operator == "*":
+        return multiply(a, b)
+    elif operator == "/":
+        return divide(a, b)
+    else:
+        return "Invalid operation!"
