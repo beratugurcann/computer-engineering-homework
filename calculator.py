7283aafcd36fb4b794d1a14c91df349ad1aab985
beratugurcann <beratugurcann@gmail.com>
2025-10-23 10:26:12 +0300

    son güncelleme

--- calculator.py
+++ calculator.py
@@ -1,3 +1,36 @@
+import os
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def result(a, b, operator):
+    if operator == "+":
+        return add(a, b)
+    elif operator == "-":
+        return subtract(a, b)
+    elif operator == "*":
+        return multiply(a, b)
+    elif operator == "/":
+        return divide(a, b)
+    else:
+        return "Invalid operation!"
+    
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b!=0:
+        return a/b
+    else:
+        print("Cannot divide by zero!!")
+        return None
+    
 while True:
     select = input("enter: process,'C': clear,'Q': exit").lower()
 
@@ -12,26 +45,6 @@ while True:
         first_value = float(input("Enter a number:"))
         process = input("Performed process (+, -, *, /):")
         second_value = float(input("Enter the second number:"))
-        print("Result:",result(first_value,process,second_value))
+        print("Result:",result(first_value,second_value,process))
     except ValueError:
         print("Please enter a valid number!!")
-
-
-    import os
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-    
-import os 
-        def result(a, b, operator):
-    if operator == "+":
-        return add(a, b)
-    elif operator == "-":
-        return subtract(a, b)
-    elif operator == "*":
-        return multiply(a, b)
-    elif operator == "/":
-        return divide(a, b)
-    else:
-        return "Invalid operation!"
